d03f638a0f096f6efa53dfc03da03f708adaa7cd
AmanSoni1-apex <226853465+AmanSoni1-apex@users.noreply.github.com>
2026-04-27 13:48:12 +0530

    Add the CORS middleware setup

--- app/main.py
+++ app/main.py
@@ -24,6 +24,20 @@ app=FastAPI(
     description="Transforming messy ideas into technical blueprints.",
     version="1.0.0"
 )
+# CORS (Cross-Origin Resource Sharing) Middleware setup , this allow our frontend which is running on the different port to communicate with our backend securely . 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    # allow_origins=["*"] -> This allows requests from ANY domain, if you change it to specific domain then it will only allow the requests from that domain only which we do in the production environment. In production we should not allow all the origins becuase it is a security risk
+
+    allow_credentials=True,
+    #  Allow the browsers to send cookies and auth headers which is required for the authentication if this is false then the browser will not send any cookies and auth headers to the backend and now there will be a problem of user log-out in our application .
+
+
+    allow_methods=["*"], #all the methods are allowed (GET ,POST DELETE, UPDATE)
+
+    allow_headers=["*"], # Allows all metadata/headers to be sent by the frontend ( like content-type ,accept etc )
+    )
 
 app.include_router(
     problems.router,
