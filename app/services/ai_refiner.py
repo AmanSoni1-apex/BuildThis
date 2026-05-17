04de915312a7f2be04ace10c220bc3cebc8a072d
AmanSoni1-apex <226853465+AmanSoni1-apex@users.noreply.github.com>
2026-05-17 14:11:17 +0530

    done some changes into the ai refinement module specifically change the ai model from nvidia nemotron to llama

--- app/services/ai_refiner.py
+++ app/services/ai_refiner.py
@@ -12,24 +12,23 @@ class ProblemRefined(BaseModel):
     category: str = Field(description="The category of the problem (e.g. Transport, Food, Health)")
 
 load_dotenv()
-api_key=os.getenv("OPENROUTER_API_KEY")
+api_key=os.getenv("GROQ_API_KEY")
 
 # here we are making a llm object so that we can send the data to the llm for the further processing
 llm=ChatOpenAI(
-    model="nvidia/nemotron-3-super-120b-a12b:free",
+    model="llama-3.3-70b-versatile",
     api_key=api_key,
-    base_url="https://openrouter.ai/api/v1",
+    base_url="https://api.groq.com/openai/v1",
     temperature=0.7
 )
 
-
- # This is used to parse the output of the llm into a json format 
+# This is used to parse the output of the llm into a json format 
 parser = JsonOutputParser(pydantic_object=ProblemRefined)
 
 # Now we are creating a prompt template so that we can able to set the behaviour of the llm and also we can able to pass the input to the llm 
 prompt_Template=ChatPromptTemplate.from_messages([
     ('system','You are an expert Solution Architect. Your task is to take a messy, unstructured problem description from a user and refine it into a clear, technical problem statement. Do not add any extra information or explaination in the output return only the valid JSON output {format_instructions} '),
-    ('human',"{messy_input}")
+    ('human',"{messy_input}") 
 ])
 
 prompt_Template = prompt_Template.partial(format_instructions=parser.get_format_instructions())
